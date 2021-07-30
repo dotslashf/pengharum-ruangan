d68b43c65dd29ee40f9116a1938bce588cbdefbb
dotslashf <fadhlurahman.i@gmail.com>
2021-07-30 19:38:15 +0800

    Fix index

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,25 @@ import Randomizer from './lib/Randomizer';
 import Formatter from './lib/Formatter';
 import VideoMaker from './lib/VideoMaker';
 import Twitter from './lib/Twitter';
+import sleep from './utils/sleep';
 
 (async () => {
-  const r = new Randomizer();
-  const generatedAudioData = r.audioData;
-  // const generatedImgSrc = await r.generatedImgSrc;
+  while (true) {
+    const r = new Randomizer();
+    const audioData = r.getAudioData();
+    const generatedImgSrc = await r.generatedImgSrc;
 
-  const f = new Formatter(generatedAudioData);
-  const tweet = f.finalText;
-  console.log(tweet); 
+    const f = new Formatter(audioData);
+    const tweet = f.finalText;
 
-  // const vm = new VideoMaker(generatedImgSrc, generatedAudioData);
-  // await vm.generateAudio();
-  // const videoPath = await vm.generateVideo();
+    const vm = new VideoMaker(generatedImgSrc, audioData);
+    await vm.generateAudio();
+    const videoPath = await vm.generateVideo();
 
-  // const twitter = new Twitter();
-  // const mediaId = await twitter.uploadVideo(videoPath);
-  // await twitter.tweetVideo(mediaId, tweet);
+    const twitter = new Twitter();
+    const mediaId = await twitter.uploadVideo(videoPath);
+    await twitter.tweetVideo(mediaId, tweet);
+
+    await sleep(30);
+  }
 })();
